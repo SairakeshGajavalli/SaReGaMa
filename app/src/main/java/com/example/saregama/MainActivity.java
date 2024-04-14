00eb396ab4e0ba6270eda67fbd8047994458e31c
SairakeshGajavalli <gajavallisairakesh1999@gmail.com>
2024-04-14 11:19:10 -0500

    Update on Main activity

--- app/src/main/java/com/example/saregama/MainActivity.java
+++ app/src/main/java/com/example/saregama/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.saregama;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button continueButton = findViewById(R.id.continueBT);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SplashScreenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
