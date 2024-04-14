4710637741c052addcc4001fdb1cd80b915260fd
SairakeshGajavalli <gajavallisairakesh1999@gmail.com>
2024-04-14 12:21:04 -0500

    Connected to Firebase and created login register page
    Connected to Firebase and created login register page
    
    Co-Authored-By: BandiRamNiteeshReddy <143049772+BandiRamNiteeshReddy@users.noreply.github.com>
    Co-Authored-By: BHANU PRAKASH BATHINI <89291543+Bhanu20022003@users.noreply.github.com>
    Co-Authored-By: sathvikrdy09 <143148434+sathvikrdy09@users.noreply.github.com>

---
+++ app/src/main/java/com/example/saregama/SplashScreenActivity.java
@@ -0,0 +1,55 @@
+package com.example.saregama;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+        if(user == null){
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            textView.setText(user.getEmail());
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        ImageView continueButton = findViewById(R.id.melodify_Image);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
